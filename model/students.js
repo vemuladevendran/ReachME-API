253f511879016d1dae58321ca844d205e09d7fb6
devendran <devendranvemula@gmail.com>
2021-05-12 10:22:16 +0530

    clean up

--- model/students.js
+++ model/students.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
+const StudentSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     address: { type: String, required: true },
 });
 
-const User = mongoose.model('User', UserSchema);
+const Student = mongoose.model('Student', StudentSchema);
 
 
-module.exports = User;
+module.exports = Student;
