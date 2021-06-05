6500d27b9ef805b5742d9e77122335d00a48215c
devendran <devendranvemula@gmail.com>
2021-06-05 21:18:58 +0530

    port taken from env

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.post('/api/v1/verify', async (req, res) => {
 
 
 
-const PORT = 5000;
+const port = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`server is listering on the PORT ${PORT}`);
